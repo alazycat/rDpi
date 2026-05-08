c698ad6ab4316f48f1bdb3429da1750b30014aa9
netmare <jinzhengda12@163.com>
2026-05-08 19:57:05 +0800

    feat(protocols): add protocol registry and Detector entry point

--- target/debug/deps/rdpi-1a849baf2a6d0fe3.d
+++ target/debug/deps/rdpi-1a849baf2a6d0fe3.d
@@ -1,16 +1,17 @@
-D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.d: src\lib.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\error.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs
+D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.d: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
 
-D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.exe: src\lib.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\error.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs
+D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.exe: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
 
 src\lib.rs:
+src\error.rs:
 src\core\mod.rs:
 src\core\types.rs:
 src\core\flow.rs:
 src\core\stats.rs:
-src\error.rs:
 src\parser\mod.rs:
 src\parser\ethernet.rs:
 src\parser\ip.rs:
 src\parser\tcp.rs:
 src\parser\udp.rs:
 src\protocols\mod.rs:
+src\protocols\dns.rs:
---
+++ target/debug/deps/test_dns-f4e79009696eea69.d
@@ -0,0 +1,5 @@
+D:\rust_code\rDpi\target\debug\deps\test_dns-f4e79009696eea69.d: tests\test_dns.rs
+
+D:\rust_code\rDpi\target\debug\deps\test_dns-f4e79009696eea69.exe: tests\test_dns.rs
+
+tests\test_dns.rs:
