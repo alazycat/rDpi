1c4905cf6a602876488ef4f51277806261a319ec
netmare <jinzhengda12@163.com>
2026-05-08 20:03:12 +0800

    feat(dns): add DNS protocol detector

--- target/debug/deps/rdpi-1a849baf2a6d0fe3.d
+++ target/debug/deps/rdpi-1a849baf2a6d0fe3.d
@@ -1,6 +1,6 @@
-D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.d: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
+D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.d: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns\mod.rs src\protocols\dns\parser.rs src\protocols\dns\detector.rs
 
-D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.exe: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
+D:\rust_code\rDpi\target\debug\deps\rdpi-1a849baf2a6d0fe3.exe: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns\mod.rs src\protocols\dns\parser.rs src\protocols\dns\detector.rs
 
 src\lib.rs:
 src\error.rs:
@@ -14,4 +14,6 @@ src\parser\ip.rs:
 src\parser\tcp.rs:
 src\parser\udp.rs:
 src\protocols\mod.rs:
-src\protocols\dns.rs:
+src\protocols\dns\mod.rs:
+src\protocols\dns\parser.rs:
+src\protocols\dns\detector.rs:
--- target/debug/deps/rdpi-b13252b84b483ed0.d
+++ target/debug/deps/rdpi-b13252b84b483ed0.d
@@ -1,8 +1,8 @@
-D:\rust_code\rDpi\target\debug\deps\rdpi-b13252b84b483ed0.d: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
+D:\rust_code\rDpi\target\debug\deps\rdpi-b13252b84b483ed0.d: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns\mod.rs src\protocols\dns\parser.rs src\protocols\dns\detector.rs
 
-D:\rust_code\rDpi\target\debug\deps\librdpi-b13252b84b483ed0.rlib: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
+D:\rust_code\rDpi\target\debug\deps\librdpi-b13252b84b483ed0.rlib: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns\mod.rs src\protocols\dns\parser.rs src\protocols\dns\detector.rs
 
-D:\rust_code\rDpi\target\debug\deps\librdpi-b13252b84b483ed0.rmeta: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns.rs
+D:\rust_code\rDpi\target\debug\deps\librdpi-b13252b84b483ed0.rmeta: src\lib.rs src\error.rs src\core\mod.rs src\core\types.rs src\core\flow.rs src\core\stats.rs src\parser\mod.rs src\parser\ethernet.rs src\parser\ip.rs src\parser\tcp.rs src\parser\udp.rs src\protocols\mod.rs src\protocols\dns\mod.rs src\protocols\dns\parser.rs src\protocols\dns\detector.rs
 
 src\lib.rs:
 src\error.rs:
@@ -16,4 +16,6 @@ src\parser\ip.rs:
 src\parser\tcp.rs:
 src\parser\udp.rs:
 src\protocols\mod.rs:
-src\protocols\dns.rs:
+src\protocols\dns\mod.rs:
+src\protocols\dns\parser.rs:
+src\protocols\dns\detector.rs:
